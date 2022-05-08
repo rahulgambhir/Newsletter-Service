2d9ac68043cdbb312dac2f89091402b2700bc97d
Rahul Gambhir <gambhirrahul0@gmail.com>
2022-05-08 15:22:22 +0530

    Minor fix

--- routes/email.js
+++ routes/email.js
@@ -5,7 +5,7 @@ const cron = require("node-cron");
 const { email } = require("../controllers/emailController");
 
 // Will call the email() every hour
-cron.schedule("* * * *", () => {
+cron.schedule("0 * * * *", () => {
   email();
 });
 
